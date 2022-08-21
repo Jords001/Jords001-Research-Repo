f4c990911cd713b4babe06ff9c33ac8fe6bd3dbc
Jords001 <68764249+Jords001@users.noreply.github.com>
2022-08-21 20:37:35 +1200

    Update Seventh py project add10int and average.py

--- myPython/Seventh py project add10int and average.py
+++ myPython/Seventh py project add10int and average.py
@@ -1,8 +1,8 @@
 list = []
-numberOfInt = 10
+numberOfInt = 10 ## Variable that contains the max for the numbers list range
 for n in range(numberOfInt):
-    numbers = int(input('Enter number: '))
-    list.append(numbers)
-average = sum(list) / len(list)
+    numbers = int(input('Enter number: '))## This input will occur 10x
+    list.append(numbers) ## appends an element to the end of the list can be str number object ect.
+average = sum(list) / len(list) ## This variable adds the list together and then divides it by the amount of items in the list
 print(sum(list))
 print(average)
