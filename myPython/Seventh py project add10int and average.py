2c6fdeb4fbfb03dd8e346a7c3e5b873cebd68e41
Jords001 <68764249+Jords001@users.noreply.github.com>
2022-09-01 09:16:22 +1200

    Update Seventh py project add10int and average.py

--- myPython/Seventh py project add10int and average.py
+++ myPython/Seventh py project add10int and average.py
@@ -4,5 +4,5 @@ for n in range(numberOfInt):
     numbers = int(input('Enter number: '))## This input will occur 10x
     list.append(numbers) ## appends an element to the end of the list can be str number object ect.
 average = sum(list) / len(list) ## This variable adds the list together and then divides it by the amount of items in the list
-print(sum(list))
-print(average)
+print(sum(list)) ## This will print just the sum of the list (added together)
+print(average) ## This will print the numbers that are averaged,(added together and divided)
