d1dfb42bd54ba44297b0fe946b3c97c3dac15ccb
Jords001 <68764249+Jords001@users.noreply.github.com>
2022-09-01 09:58:02 +1200

    Update Fourth Py Project Is It an Integer.py

--- myPython/Fourth Py Project Is It an Integer.py
+++ myPython/Fourth Py Project Is It an Integer.py
@@ -1,15 +1,15 @@
 isItAninteger = input("input a string: ")
 
-def isFloat(num):
+def isFloat(num): ## Define isFloat
     try:
-        float(num)
+        float(num) ##Try convert to float
         return "is a float"
-    except ValueError:
+    except ValueError:## Exception return nothing
         return ""
 
 if isItAninteger.isdigit(): ## .isdigit checks to see if it is a decimal number and then proceeds to print This string is an integer
     print("This string is an integer")
-elif isFloat(isItAninteger):
+elif isFloat(isItAninteger): ## As defined isFloat checks to see if the string is a float
     print("This string is a float")
-else:
+else: ## Otherwise print this is not and integer
     print("This is not an integer")
