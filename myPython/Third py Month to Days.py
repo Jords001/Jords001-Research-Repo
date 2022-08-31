45bf3c0e0b97a77c683ce2a097a345ee8700a65c
Jords001 <68764249+Jords001@users.noreply.github.com>
2022-09-01 09:59:59 +1200

    Update Third py Month to Days.py

--- myPython/Third py Month to Days.py
+++ myPython/Third py Month to Days.py
@@ -3,13 +3,13 @@
 month = input('''Insert month shorthand eg Jan Feb Mar
 : ''')
 
-if month in ("Jan","jan","Mar", "mar","May","may","Jul","jul","Aug","aug","Oct","oct","Dec","dec"): 
+if month in ("Jan","jan","Mar", "mar","May","may","Jul","jul","Aug","aug","Oct","oct","Dec","dec"): ##if input variable month is in array dayNum=31
     dayNum = 31
-elif month in ("Apr","apr","Jun","jun","Sep","sep","Nov","nov",):
+elif month in ("Apr","apr","Jun","jun","Sep","sep","Nov","nov",): ##if input variable month is in array dayNum=30
     dayNum = 30
-else:
+else: ##Otherwise dayNum = 28
     dayNum = 28
 
-print (dayNum)
+print (dayNum) ##Print dayNum
 
 ## Initially didn't use in on line 6 and 8 and so the code didn't work as intended as it was looking for the whole array
