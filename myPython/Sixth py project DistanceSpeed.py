511d7047dee54094ed7979c861d471af927be472
Jords001 <68764249+Jords001@users.noreply.github.com>
2022-09-01 09:26:00 +1200

    Update Sixth py project DistanceSpeed.py

--- myPython/Sixth py project DistanceSpeed.py
+++ myPython/Sixth py project DistanceSpeed.py
@@ -1,19 +1,19 @@
-distance = float(input("Distance meters: "))
-timeHours = float(input("Time Hours: "))
-timeMinutes = float(input("Time Minutes: "))
-timeSeconds = float(input("Time Seconds: "))
+distance = float(input("Distance meters: ")) ##Input variable for distance in meters
+timeHours = float(input("Time Hours: ")) ##Input variable for time in hours
+timeMinutes = float(input("Time Minutes: ")) ##Input variable for time in minutes
+timeSeconds = float(input("Time Seconds: ")) ##Input variable for time in seconds
 
-timeHoursSeconds = timeHours * 3600
-timeMinutesSeconds = timeMinutes * 60
-timeMinutesHours = timeMinutes / 60
-timeSecondsHours = timeSeconds /3600
-totalSeconds = timeSeconds + timeMinutesSeconds + timeHoursSeconds
-totalHours = timeHours + timeSecondsHours + timeMinutesHours
-mps = distance / totalSeconds
-distanceKm = distance / 1000
-kmph = distanceKm / totalHours
-distanceMi = distanceKm * 0.621371
-miph = distanceMi / totalHours
+timeHoursSeconds = timeHours * 3600 ##Convers hours to seconds
+timeMinutesSeconds = timeMinutes * 60 ## Converts seconds to minutes
+timeMinutesHours = timeMinutes / 60 ##converts hours to minutes
+timeSecondsHours = timeSeconds /3600 ##converts seconds to hours
+totalSeconds = timeSeconds + timeMinutesSeconds + timeHoursSeconds ##Add all seconds
+totalHours = timeHours + timeSecondsHours + timeMinutesHours ##Add all hours
+mps = distance / totalSeconds ## Meters per second, distance divided by total seconds
+distanceKm = distance / 1000 ## Distance of meters divided by 1000 to create km
+kmph = distanceKm / totalHours ##Distance kilometers per hour, distance km divided by total hours
+distanceMi = distanceKm * 0.621371 ##Times km distance by 0.621371 to get miles per hour
+miph = distanceMi / totalHours ##Miles per hour is distance in miles divided by total hours
 
 print("Your speed in mps =" + str(mps))
 print("Your speed in kmph =" + str(kmph))
